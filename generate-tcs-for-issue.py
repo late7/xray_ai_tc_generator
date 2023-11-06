6a24c47835a2938cb30b89eecff8d374a1493e45
Lasse Mikkonen <lasse.mikkonen@gmail.com>
2023-11-06 22:13:21 +0200

    Added Test Case amount parameter. Added Template files for API keys.

--- generate-tcs-for-issue.py
+++ generate-tcs-for-issue.py
@@ -38,22 +38,27 @@ def get_issue_data(issue_key, BASE_URL, USERNAME):
         print(response.text)
         return None
 
-def generate_testcases(req_data, debug):
+def generate_testcases(req_data, tc_amount, debug):
     print("Let's generate Test Cases: ")
     if debug: print(req_data)
-    ai_data = generate_with_openai(req_data, debug)
+    ai_data = generate_with_openai(req_data, tc_amount, debug)
     if debug: print(ai_data)
  
-def generate_with_openai(prompt, debug):
+def generate_with_openai(prompt, tc_amount, debug):
     with open(OPENAI_TOKEN_FILE, 'r') as file:
         openai.api_key = file.readline().strip()
+
+    # Create Prompt
+    # tc_amount = 2
+    system_prompt = "Define " + str(tc_amount) + " Test Cases for the provided requirement specification. it. Note: Response must be in plain json format.\n\nJson format to use:\n\n[\n    {\n        \"testtype\": \"Manual\",\n        \"fields\": {\n            \"summary\": \"Test Case 1: Minimum Speed Test\",\n            \"description\": \"Objective: To verify that...\\n Preconditions: ...\"\n        },\n        \"steps\": [\n            {\n                \"action\": \"Begin a data fetch to ...\",\n                \"data\": \"\",\n                \"result\": \"Data fetch operation must be completed...\"\n            },\n        ]\n    }\n]"
     response = openai.ChatCompletion.create(
       model= AI_MODEL,
       messages=[
           {
           "role": "system",
   
-          "content": "Define 2 Test Cases for the provided requirement specification. it. Note: Response must be in plain json format.\n\nJson format to use:\n\n[\n    {\n        \"testtype\": \"Manual\",\n        \"fields\": {\n            \"summary\": \"Test Case 1: Minimum Speed Test\",\n            \"description\": \"Objective: To verify that...\\n Preconditions: ...\"\n        },\n        \"steps\": [\n            {\n                \"action\": \"Begin a data fetch to ...\",\n                \"data\": \"\",\n                \"result\": \"Data fetch operation must be completed...\"\n            },\n        ]\n    }\n]"
+          # "content": "Define 2 Test Cases for the provided requirement specification. it. Note: Response must be in plain json format.\n\nJson format to use:\n\n[\n    {\n        \"testtype\": \"Manual\",\n        \"fields\": {\n            \"summary\": \"Test Case 1: Minimum Speed Test\",\n            \"description\": \"Objective: To verify that...\\n Preconditions: ...\"\n        },\n        \"steps\": [\n            {\n                \"action\": \"Begin a data fetch to ...\",\n                \"data\": \"\",\n                \"result\": \"Data fetch operation must be completed...\"\n            },\n        ]\n    }\n]"
+          "content": system_prompt
           },
           {
           "role": "user",
@@ -123,7 +128,7 @@ def import_test_cases_to_xray(JIRA_PARENT_ISSUE, debug):
         print(data)
 
         
-def main(req, username, baseurl, debug):
+def main(req, username, baseurl, tc_amount, debug):
     url = baseurl + '/rest/api/2'
     # ISSUE_KEY = input("Enter the IssueID for Requirement : ")
     issue_data = get_issue_data(req, url, username)
@@ -141,7 +146,7 @@ def main(req, username, baseurl, debug):
         print("No love. Issue not found for id: " + req)
         exit()
     if input("Generate Test Cases: y/n?") == "y":
-        generate_testcases(req_data, debug)
+        generate_testcases(req_data, tc_amount, debug)
         if input("Import Test Cases to XRAY: y/n?") == "y":
             import_test_cases_to_xray(req, debug)
     else:
@@ -153,10 +158,11 @@ if __name__ == "__main__":
     parser.add_argument('--req', required=True, help='Requirement Issue ID.')
     parser.add_argument('--username', required=True, help='Jira user name for login.')
     parser.add_argument('--url', required=True, help='Jira Cloud URL.')
+    parser.add_argument('--tc_amount', type=int, default=1, help='Amount of Test Cases to generate. Default is 1.')
     parser.add_argument('--debug', type=bool, default=False, help='Debug flag. Default is False.')
 
     args = parser.parse_args()
 
-    main(args.req, args.username, args.url, args.debug)
+    main(args.req, args.username, args.url, args.tc_amount, args.debug)
 
 
